6cb25abefe27330b67bf6f8f74fadf6b9258adbf
Adrianabalcd <adribalcd@gmail.com>
2024-02-11 21:52:23 -0500

    Refactor: Añadiendo lógica según entorno

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,23 +2,21 @@ const Joi = require('@hapi/joi')
 const axios = require('axios')
 
 exports.paramCaptcha = async (ctx, next) => {
-	let RECAPTCHA_KEY= process.env.RECAPTCHA_KEY
-	
 	const res = Joi.string().required().validate(ctx.request.body.captcha);
-	if (res.error)
-		console.log("Datos invalidos")
+	
+	if (process.env.ENVIROMENT != 'development') {
+		if (res.error)
+			console.log("Datos invalidos")
 
-	const secret = RECAPTCHA_KEY;
-	const response = ctx.request.body.captcha || null
-	console.log("response",response)
-	console.log("response length",response.length)
-	let { data } = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${response}`,
-		{}
-	);
+		const secret = process.env.RECAPTCHA_KEY;
+		const response = ctx.request.body.captcha || null
+		
+		let { data } = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${response}`,
+			{}
+		);
 
-	console.log("data", data)
-	if (!data.success)
-        console.log("Datos invalidos")
-
+		if (!data.success)
+			console.log("Datos invalidos")
+	}
 	return next();
 }
