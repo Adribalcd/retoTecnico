79dad1e097bff4e6c7692f2dc8316e8b1a5ee8ae
Adrianabalcd <adribalcd@gmail.com>
2024-02-11 21:49:30 -0500

    refactor: Eliminando clases no usadas en Errors.js

--- helpers/Errors.js
+++ helpers/Errors.js
@@ -1,24 +1,11 @@
 exports.Mistake = class Mistake extends Error {
 	/**
-	 * Every error thorown as it is, is meant to reach the final user
-	 * @param {String} message - Message to the client
+	 * @param {String} message
 	 */
 	constructor(status, message) {
 		super()
 		this.name = 'mistake'
 		this.status = status
-		this.message = message
-	}
-}
-
-exports.Forbidden = class Forbidden extends Error {
-	/**
-	 * Every error thorown as it is, is meant to reach the final user
-	 * @param {String} message - Message to the client
-	 */
-	constructor(message) {
-		super()
-		this.name = 'forbidden'
 		this.message = message
 	}
 }
